b1bab9549b7f97b8ba16735e3b7070b0d52e29d1
xccyxx <100075841+xccyxx@users.noreply.github.com>
2024-08-19 00:15:23 -0400

    Add pixelate grids on hover feature

--- scripts.js
+++ scripts.js
@@ -6,15 +6,25 @@ const createDiv = (number) => {
         let row = document.createElement("div");
         row.classList.add("row");
         container.appendChild(row);
-        // Create cols inside each rows
-        for (let i = 0; i < number; i++) { 
-            let col = document.createElement("div");
-            col.classList.add("col");
-            row.appendChild(col);
+        // Create grids inside each rows
+        for (let j = 0; j < number; j++) { 
+            let grid = document.createElement("div");
+            grid.classList.add("grid");
+            row.appendChild(grid);
         }
     }
 }
 
+const pixelateGridsOnHover = () => {
+    let container = document.querySelector(".container");
+    container.addEventListener("mouseover", event => {
+        if (event.target.className === "grid") {
+            event.target.style.backgroundColor = "#404040";
+        }
+    })
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     createDiv(16);
+    pixelateGridsOnHover();
 });
