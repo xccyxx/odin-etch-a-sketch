f1f79b4bcc203cd51481866559e4a696766f43ef
xccyxx <100075841+xccyxx@users.noreply.github.com>
2024-08-19 02:08:09 -0400

    Add feature of setting number of squares per side to the button

--- scripts.js
+++ scripts.js
@@ -1,30 +1,62 @@
-const createDiv = (number) => {
+const createCell = (number) => {
     let container = document.querySelector(".container");
+    container.innerHTML = '';
     // Create 16x16 divs using JavaScript
     for (let i = 0; i < number; i++) {
         // Create rows
         let row = document.createElement("div");
         row.classList.add("row");
         container.appendChild(row);
-        // Create grids inside each rows
+        // Create cells inside each rows
         for (let j = 0; j < number; j++) { 
-            let grid = document.createElement("div");
-            grid.classList.add("grid");
-            row.appendChild(grid);
+            let cell = document.createElement("div");
+            cell.classList.add("cell");
+            row.appendChild(cell);
         }
     }
 }
 
-const pixelateGridsOnHover = () => {
+const pixelateCellsOnHover = () => {
     let container = document.querySelector(".container");
+    // event delegation
     container.addEventListener("mouseover", event => {
-        if (event.target.className === "grid") {
+        if (event.target.className === "cell") {
             event.target.style.backgroundColor = "#404040";
         }
     })
 }
 
+
+const promptForSideSquaresNumber = () => {
+    let input;
+    input = prompt("Please enter the number of squares per side: (1-100)");
+    // handle cancellation
+    if (input === null) {
+        return null;
+    }
+    let sideSquaresNumber = +input;
+    // handle invalid input
+    if (isNaN(sideSquaresNumber) || sideSquaresNumber <= 0 || sideSquaresNumber > 100) {
+        alert("Invalid number. Please try again.");
+        return null;
+    }
+    return sideSquaresNumber;
+
+}
+
+const setupGridSizeButton = () => {
+    let button = document.querySelector(".button");
+    button.addEventListener("click", () => {
+        let gridSize = promptForSideSquaresNumber();
+        // avoid setting grid size using a invalid number
+        if (gridSize !== null) {
+            createCell(gridSize);
+        }
+    })
+}
+
 document.addEventListener("DOMContentLoaded", () => {
-    createDiv(16);
-    pixelateGridsOnHover();
+    createCell(16);
+    pixelateCellsOnHover();
+    setupGridSizeButton();
 });
